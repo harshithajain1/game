0630c77edf8e4e4c1fa1358bf2113636f1064e91
harshithajain1 <157448253+harshithajain1@users.noreply.github.com>
2024-03-14 10:30:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const buttons = document.querySelectorAll("button");
+    const result = document.getElementById("result");
+  
+    buttons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const userChoice = e.target.id;
+        const computerChoice = choices[Math.floor(Math.random() * 3)].toLowerCase();
+  
+        if (userChoice === computerChoice) {
+          result.textContent = "It's a tie!";
+        } else if (
+          (userChoice === "rock" && computerChoice === "scissors") ||
+          (userChoice === "scissors" && computerChoice === "paper") ||
+          (userChoice === "paper" && computerChoice === "rock")
+        ) {
+          result.textContent = `You win! Computer choose ${computerChoice}.`;
+        } else {
+          result.textContent = `You lose! Computer choose ${computerChoice}.`;
+        }
+      });
+    });
+  });
+  
